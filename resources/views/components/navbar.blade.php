e1926675a5b5fc1f797541abd711e9dedaf40662
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-26 22:50:41 +0700

    set navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="w-full bg-white border-b h-20">
+    <div class="flex justify-end items-center h-full">
+        <div class="mr-20 divide-x flex items-center">
+            <div class="font-medium pr-3">Aditya</div>
+            <div class="font-medium pl-3">
+                <div class="flex items-center gap-2 bg-gradient-to-r from-emerald-700 to-blue-600 rounded-lg px-3 py-1.5 hover:brightness-90 hover:cursor-pointer">
+                    <span class="text-white text-sm">Logout</span>                              
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
