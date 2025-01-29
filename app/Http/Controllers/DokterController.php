a0f06d3d291d56784a53e360e2da736387df837b
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-29 17:25:52 +0700

    dokter controller

---
+++ app/Http/Controllers/DokterController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dokter;
+use Illuminate\Http\Request;
+
+class DokterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return view('dokter.dokter');
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dokter $dokter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dokter $dokter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dokter $dokter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dokter $dokter)
+    {
+        //
+    }
+}
