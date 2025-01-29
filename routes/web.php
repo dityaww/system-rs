593f96aec52737d86aa7af60da7759c79994bdfb
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-29 16:19:34 +0700

    update routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Dashboard;
+use App\Http\Controllers\DokterController;
 use App\Http\Controllers\PasienController;
+use App\Http\Controllers\PoliController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,3 +18,15 @@ Route::post('/pasien/store', [PasienController::class, 'store'])->name('pasien.s
 Route::get('/pasien/edit/{id}', [PasienController::class, 'edit'])->name('pasien.edit');
 Route::post('/pasien/update/{id}', [PasienController::class, 'update'])->name('pasien.update');
 Route::post('/pasien/delete/{id}', [PasienController::class, 'delete'])->name('pasien.delete');
+
+Route::get('/dokter', [DokterController::class, 'index'])->name('dokter.index');
+
+Route::get('/poli', [PoliController::class, 'index'])->name('poli.index');
+Route::get('/poli/create', [PoliController::class, 'create'])->name('poli.create');
+Route::post('/poli/store', [PoliController::class, 'store'])->name('poli.store');
+Route::get('/poli/edit/{id}', [PoliController::class, 'edit'])->name('poli.edit');
+Route::post('/poli/update/{id}', [PoliController::class, 'update'])->name('poli.update');
+Route::post('/poli/delete/{id}', [PoliController::class, 'delete'])->name('poli.delete');
+
+
+
