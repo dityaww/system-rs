33113e43cc14b02222eaceebb4ca19be764e7db4
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-29 11:37:25 +0700

    pasien page update

--- resources/views/pasien/pasien.blade.php
+++ resources/views/pasien/pasien.blade.php
@@ -14,18 +14,25 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td class="text-center text-gray-600 py-4">1</td>
-                <td class="text-center text-gray-600 py-4">00122445</td>
-                <td class="text-center text-gray-600 py-4">Aditya</td>
-                <td class="text-center text-gray-600 py-4">aditya@gmail.com</td>
-                <td class="text-center text-gray-600 py-4">Laki-Laki</td>
-                <td class="text-center text-gray-600 py-4">Belum Menikah</td>
-                <td class="text-center text-gray-600 py-4">
-                    <a class="hover:bg-blue-600 bg-blue-500 text-white px-3 py-1 rounded-md text-sm" href="{{ route('pasien.edit') }}">Edit</a>
-                    <a class="hover:bg-red-600 bg-red-500 text-white px-3 py-1 rounded-md text-sm" href="">Delete</a>
-                </td>
-            </tr>
+            @foreach($pasien as $no=>$data)
+                <tr>
+                    <td class="text-center text-gray-600 py-4">{{ $no+1 }}</td>
+                    <td class="text-center text-gray-600 py-4">{{ $data->no_rm }}</td>
+                    <td class="text-center text-gray-600 py-4">{{ $data->nama }}</td>
+                    <td class="text-center text-gray-600 py-4">{{ $data->email }}</td>
+                    <td class="text-center text-gray-600 py-4">{{ $data->jenis_kelamin }}</td>
+                    <td class="text-center text-gray-600 py-4">{{ $data->status_nikah }}</td>
+                    <td class="text-center text-gray-600 py-4">
+                        <div class="flex gap-2 justify-center">
+                            <a class="hover:bg-blue-600 bg-blue-500 text-white px-3 py-1 rounded-md text-sm" href="{{ route('pasien.edit', $data->id) }}">Edit</a>
+                            <form action="{{ route('pasien.delete', $data->id) }}" method="post">
+                                @csrf
+                                <button class="hover:bg-red-600 bg-red-500 text-white px-3 py-1 rounded-md text-sm">Delete</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 </x-dashboard>
