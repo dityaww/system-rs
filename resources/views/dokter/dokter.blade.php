eb8c97d8a114f62b56966cb13178fb75b256ed6b
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-29 17:25:33 +0700

    view: tabel dokter

---
+++ resources/views/dokter/dokter.blade.php
@@ -0,0 +1,17 @@
+<x-dashboard>
+    <h1 class="text-3xl font-semibold">Data Dokter</h1>
+
+    <table class="mt-10 w-full">
+        <thead class="bg-gray-50">
+            <tr>
+                <th class="py-3 text-center font-medium text-gray-700">No</th>
+                <th class="py-3 text-center font-medium text-gray-700">Nama</th>
+                <th class="py-3 text-center font-medium text-gray-700">Spesialis</th>
+                <th class="py-3 text-center font-medium text-gray-700">Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
+</x-dashboard>
